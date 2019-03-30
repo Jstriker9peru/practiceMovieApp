11a32efbaaa6150b0cd928a2293079e0155212e6
Jesp <jstriker9peru@gmail.com>
2019-03-30 15:30:01 -0400

    Filled in movie page

--- src/components/elements/MovieThumb/MovieThumb.js
+++ src/components/elements/MovieThumb/MovieThumb.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 import './MovieThumb.css';
 
 const MovieThumb = (props) => {
@@ -12,6 +13,12 @@ const MovieThumb = (props) => {
             : <img src={props.image} alt="moviethumb" /> }
         </div>
     )
+}
+
+MovieThumb.propTypes = {
+    image: PropTypes.string,
+    movieId: PropTypes.number,
+    movieName: PropTypes.string
 }
 
 export default MovieThumb;
